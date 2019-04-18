2421023528a0c1deb6f1aa96635c2ad1b6e495ad
mechandesuw <w.mick_ovo@ezweb.ne.jp>
2019-04-19 05:13:44 +0900

    aaaaaaaa

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    var playerView: UIView!
+    var playerMotionManager: CMMotionManager!
+    var speedX: Double = 0.0
+    var speedY: Double = 0.0
+   
     //画面サイズの取得
     let screenSize = UIScreen.main.bounds.size
     
@@ -46,7 +53,90 @@ class ViewController: UIViewController {
         view.center = center
         
         return view
+        
     }
+
+        
+    func startAccelerometer() {
+     
+            //加速度を取得する
+         let handler: CMAccelerometerHandler = {(CMAccelerometerData:CMAccelerometerData?, error:Error?) -> Void in
+            self.speedX += CMAccelerometerData!.acceleration.x
+            self.speedY += CMAccelerometerData!.acceleration.y
+            
+            //プレイヤーの中心位置を設定
+            var posX = self.playerView.center.x + (CGFloat(self.speedX) / 3)
+            var posY = self.playerView.center.y - (CGFloat(self.speedY) / 3)
+            
+            //画面上からプレイヤーがはみ出しそうだったら、posX/posYを設定
+                if posX <= self.playerView.frame.width / 2 {
+                    self.speedX = 0
+                    posX = self.playerView.frame.width / 2
+                }
+                if posY <= self.playerView.frame.height / 2 {
+                    self.speedY = 0
+                    posY = self.playerView.frame.height / 2
+                }
+                if posX >= self.screenSize.width - (self.playerView.frame.width / 2) {
+                    self.speedX = 0
+                    posX = self.screenSize.width - (self.playerView.frame.width / 2)
+                }
+                if posY >= self.screenSize.height - (self.playerView.frame.height / 2) {
+                    self.speedY = 0
+                    posY = self.screenSize.height - (self.playerView.frame.height / 2)
+                }
+            
+            for wallRect in self.wallRectArray {
+                if (wallRect.intersects(self.playerView.frame)){
+                    self.gameCheck("gameover",massege:"壁に当たりました")
+                    return
+                }
+            }
+            
+            if (self.goalView.frame.intersects(self.playerView.frame)){
+                self.gameCheck("clear",message: "クリアしました！")
+                return
+            }
+                self.playerView.center = CGPoint(x: posX, y: posY)
+                
+            }
+        
+        func gameCheck(result: String, message: String){
+            //加速度を止める
+            if playerMotionManager.isAccelerometerActive {
+                playerMotionManager.stopAccelerometerUpdates()
+            }
+            let gameCheckAlert: UIAlertController = UIAlertController(title: result, message: message , preferredStyle:  .alert)
+            
+            let retryAction = UIAlertAction(title: "もう一度", style: .default, handler: {
+                (action:UIAlertAction!) -> Void in
+            self.retry()
+        })
+        gameCheckAlert.addAction(retryAction)
+        
+        self.present(gameCheckAlert, animated: true, completion: nil)
+        }
+        
+        func retry() {
+            //プレイヤー位置えを初期化
+            playerView.center = startView.center
+            //加速度センサーを始める
+            if !playerMotionManager.isAccelerometerActive {
+                self.startAccelerometer()
+            }
+            //スピードを初期化
+            speedX = 0.0
+            speedY = 0.0
+            }
+        }
+            //加速度の開始
+            playerMotionManager.startAccelerometerUpdates(to: OperationQueue.main, withHandler: handler)
+            
+        }
+
+    
+    //wallViewのフレームの情報を入れておく配列
+    var wallRectArray = [CGRect]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,15 +150,16 @@ class ViewController: UIViewController {
         
         for y in 0 ..< maze.count {
             for x in 0 ..< maze[y].count {
-                switch maze[x][y] {
+                switch maze[y][x] {
                 case 1: //当たるとゲームオーバーになるマス
                     let wallView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
                     wallView.backgroundColor = UIColor.black
                     view.addSubview(wallView)
+                    wallRectArray.append(wallView.frame)
                 case 2://スタート地点
                     startView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
                     startView.backgroundColor = UIColor.green
-                    view.addSubview(goalView)
+                    view.addSubview(startView)
                 case 3://ゴール地点
                     goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
                     goalView.backgroundColor = UIColor.red
@@ -78,11 +169,20 @@ class ViewController: UIViewController {
                 }
             }
         }
+        //playetViewを生成
+        playerView = UIView(frame: CGRect(x: 0, y: 0, width: cellWidth / 6, height: cellHeight / 6))
+        playerView.center = startView.center
+        playerView.backgroundColor = UIColor.gray
+        self.view.addSubview(playerView)
         
+        //Motionmanegerを生成
+        playerMotionManager = CMMotionManager()
+        playerMotionManager.accelerometerUpdateInterval = 0.02
         
+        self.startAccelerometer()
         
     }
+    
 
 
-}
 
